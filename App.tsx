a0784153de030e68ed68871cafdd37ff704e2e2e
Lars Back <lars.back@student.hs-rm.de>
2025-11-06 20:04:42 +0100

    Refactor App.tsx and apply prettier formatting

--- App.tsx
+++ App.tsx
@@ -1,26 +1,25 @@
-import { useState, useEffect } from 'react'
-import { supabase } from './lib/supabase'
-import Auth from './components/Auth'
-import Account from './components/Account'
-import { View } from 'react-native'
-import { Session } from '@supabase/supabase-js'
+import { Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import Account from "./components/Account";
+import Auth from "./components/Auth";
+import { supabase } from "./lib/supabase";
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null)
+    const [session, setSession] = useState<Session | null>(null);
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session);
+        });
 
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+        supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session);
+        });
+    }, []);
 
-  return (
-    <View>
-      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
-    </View>
-  )
+    if (session && session.user) {
+        return <Account key={session.user.id} session={session} />;
+    } else {
+        return <Auth />;
+    }
 }
