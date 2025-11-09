14aec3c15642ca6d1b200e0a12d4786e53d1120d
Katharina Loppe <Katharina.Loppe@student.hs-rm.de>
2025-11-09 19:15:26 +0100

    Fixed issue #55 (Entfernen von Daten, die aus Datenbanktabelle entfernt wurden)

--- components/Account.tsx
+++ components/Account.tsx
@@ -7,8 +7,7 @@ import { Session } from "@supabase/supabase-js";
 export default function Account({ session }: { session: Session }) {
   const [loading, setLoading] = useState(true);
   const [username, setUsername] = useState("");
-  const [website, setWebsite] = useState("");
-  const [avatarUrl, setAvatarUrl] = useState("");
+
 
   useEffect(() => {
     if (session) getProfile();
@@ -21,7 +20,7 @@ export default function Account({ session }: { session: Session }) {
 
       const { data, error, status } = await supabase
         .from("profiles")
-        .select(`username, website, avatar_url`)
+        .select(`username`)
         .eq("id", session?.user.id)
         .single();
       if (error && status !== 406) {
@@ -30,8 +29,6 @@ export default function Account({ session }: { session: Session }) {
 
       if (data) {
         setUsername(data.username);
-        setWebsite(data.website);
-        setAvatarUrl(data.avatar_url);
       }
     } catch (error) {
       if (error instanceof Error) {
@@ -44,12 +41,8 @@ export default function Account({ session }: { session: Session }) {
 
   async function updateProfile({
     username,
-    website,
-    avatar_url,
   }: {
     username: string;
-    website: string;
-    avatar_url: string;
   }) {
     try {
       setLoading(true);
@@ -58,8 +51,6 @@ export default function Account({ session }: { session: Session }) {
       const updates = {
         id: session?.user.id,
         username,
-        website,
-        avatar_url,
         updated_at: new Date(),
       };
 
@@ -87,23 +78,6 @@ export default function Account({ session }: { session: Session }) {
           label="Username"
           value={username || ""}
           onChangeText={(text) => setUsername(text)}
-        />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Input
-          label="Website"
-          value={website || ""}
-          onChangeText={(text) => setWebsite(text)}
-        />
-      </View>
-
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Button
-          title={loading ? "Loading ..." : "Update"}
-          onPress={() =>
-            updateProfile({ username, website, avatar_url: avatarUrl })
-          }
-          disabled={loading}
         />
       </View>
 
