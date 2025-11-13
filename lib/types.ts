793b2b1b96e31ee6bd0e45fd130a95b714a9f41c
Lars Back <lars.back@student.hs-rm.de>
2025-11-13 15:44:20 +0100

    feat: add Animal interface

---
+++ lib/types.ts
@@ -0,0 +1,12 @@
+export interface Animal {
+  id: string | null;
+  created_at: string | null;
+  name: string | null;
+  origin: string | null;
+  type: string | null;
+  sex: string | null;
+  size: string | null;
+  character: string | null;
+  status: string | null;
+  age: number | null;
+}
