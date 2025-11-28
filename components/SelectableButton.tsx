46456ac08432e88e14fbd2d0b37b3df25252f513
Lars Back <lars.back@student.hs-rm.de>
2025-11-28 18:01:20 +0100

    feat(style): rework selectable button

--- components/SelectableButton.tsx
+++ components/SelectableButton.tsx
@@ -1,39 +1,55 @@
+import { theme } from "@theme";
 import React from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, PressableProps, StyleSheet } from "react-native";
+import { ThemedText } from "./ThemedText";
 
-export default function SelectableButton({
-  isSelected,
-  title,
-  onPress,
-}: {
+interface SelectableButtonProps extends PressableProps {
+  backgroundColor?: string;
+  borderColor?: string;
   isSelected: boolean;
-  title: string;
-  onPress: () => void;
-}) {
-  return (
-    <Pressable onPress={onPress}>
-      <View
-        style={[
-          styles.button,
-          isSelected ? styles.selectedButton : styles.deselectedButton,
-        ]}
-      >
-        <Text>{title}</Text>
-      </View>
-    </Pressable>
-  );
 }
 
+export const SelectableButton: React.FC<
+  React.PropsWithChildren<SelectableButtonProps>
+> = ({
+  backgroundColor = theme.colors.brand.primary,
+  borderColor = theme.colors.brand.primary,
+  isSelected = false,
+  children,
+  ...props
+}) => {
+  let textColor = theme.colors.text.inverted;
+
+  if (!isSelected) {
+    textColor = theme.colors.text.dark;
+    backgroundColor = "transparent";
+    borderColor = theme.colors.brand.secondary;
+  }
+
+  return (
+    <Pressable
+      style={[
+        styles.container,
+        {
+          backgroundColor: backgroundColor,
+          borderColor,
+          borderWidth: 1,
+        },
+      ]}
+      {...props}
+    >
+      <ThemedText color={textColor} variant="buttonSecondary">
+        {children}
+      </ThemedText>
+    </Pressable>
+  );
+};
+
 const styles = StyleSheet.create({
-  button: {
-    padding: 10,
-    borderRadius: 5,
-    margin: 5,
-  },
-  selectedButton: {
-    backgroundColor: "#007AFF",
-  },
-  deselectedButton: {
-    backgroundColor: "#DDDDDD",
+  container: {
+    paddingVertical: 14,
+    paddingHorizontal: 16,
+    borderRadius: 32,
+    margin: 4,
   },
 });
--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,2 @@
 export { ThemedText } from "@components/ThemedText";
+export { SelectableButton } from "components/SelectableButton";
