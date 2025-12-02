ef6204da53880c71f5995e1c4d92a6512c4e997f
Katharina Loppe <Katharina.Loppe@student.hs-rm.de>
2025-12-02 15:10:59 +0100

    Applied Changes from Code Review

--- components/ThemedDropdown.tsx
+++ components/ThemedDropdown.tsx
@@ -3,6 +3,7 @@ import { theme } from "@theme";
 import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Dropdown } from "react-native-element-dropdown";
+import { DropdownProps } from 'react-native-element-dropdown/lib/typescript/components/Dropdown/model';
 import { EnumObject } from "../lib/supabaseEnumHandler";
 
 // dropdownData: Array of strings or EnumObject<String> (zB animalTypes)
@@ -10,20 +11,23 @@ import { EnumObject } from "../lib/supabaseEnumHandler";
 // currentVal (optional): Wert, der schon vorausgewählt sein soll
 // placeholder: String, der angezeigt wird, wenn Auswahl leer ist
 
-export default function TischuDropdown({
-  dropdownData,
-  valueSetter,
-  currentVal,
-  placeholder,
-}: {
+interface ThemedDropdownProps extends DropdownProps<any>{
   dropdownData: EnumObject<string> | string[];
   valueSetter: (item: any) => void;
-  currentVal: string | string[] | null;
-  placeholder: string | null;
-}) {
+  currentVal?: string | string[],
+  placeholder?: string,
+}
+
+export const ThemedDropdown: React.FC<ThemedDropdownProps> = ({
+  currentVal = null,
+  placeholder = null,
+  ...props
+}) => {
   const [internalVal, setInternalVal] = useState<string>();
   let data: any[];
   const IconSize = 30;
+  const DropdownS = [styles.picker, styles.dropdownMargin];
+  const containerS = [styles.picker, styles.containerMargin];
 
   const renderRightIcon = () => {
     return (
@@ -38,10 +42,10 @@ export default function TischuDropdown({
     return <View style={{ width: IconSize }} />;
   };
 
-  if (dropdownData instanceof Array) {
-    data = dropdownData;
+  if (props.dropdownData instanceof Array) {
+    data = props.dropdownData;
   } else {
-    data = dropdownData.values.map((val) => ({ value: val }));
+    data = props.dropdownData.values.map((val) => ({ value: val }));
   }
 
   return (
@@ -55,12 +59,12 @@ export default function TischuDropdown({
       renderLeftIcon={renderLeftIcon}
       placeholderStyle={styles.placeholderText}
       selectedTextStyle={styles.text}
-      containerStyle={[styles.picker, { marginTop: 5 }]}
-      style={[styles.picker, { marginVertical: 10 }]}
+      containerStyle={containerS}
+      style={DropdownS}
       itemTextStyle={styles.text}
       itemContainerStyle={styles.picker}
       onChange={(itemValue: string) => {
-        valueSetter(itemValue);
+        props.valueSetter(itemValue);
         setInternalVal(itemValue);
       }}
     />
@@ -88,4 +92,7 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     justifyContent: "center",
   },
+  dropdownMargin: {marginVertical:10},
+  containerMargin: { marginTop:5},
+
 });
